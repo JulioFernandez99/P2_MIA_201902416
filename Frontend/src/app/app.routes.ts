e007484ba9242ae0ce5a67752a2ffc7703edfde8
JulioFernandez99 <2995618790101@ingenieria.usac.edu.gt>
2024-06-22 14:35:01 -0600

    creadando dash usuario

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { RegistroComponent } from './components/auth/registro/registro.component';
+import { RegistroComponent } from './components/admin/registro/registro.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+
 
 export const routes: Routes = [
     {
-        path: 'registro',
+        path: 'registro/usuarios',
         component: RegistroComponent
     },
     {
@@ -15,5 +17,10 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'dashboard/admin',
+        component: DashboardComponent
+
     }
 ];
