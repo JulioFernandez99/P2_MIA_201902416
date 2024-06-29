c9c648c884ec902ec9d6ef035e012ddb3abdc97b
JulioFernandez99 <2995618790101@ingenieria.usac.edu.gt>
2024-06-29 17:58:26 -0600

    subiendo a vm

--- Frontend/src/environments/enviroment.ts
+++ Frontend/src/environments/enviroment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
-    
-    
-    apiUrl: 'http://localhost:3000'
-    //apiUrl: 'http://192.168.0.8:3000'
+    apiUrl: 'http://52.207.224.130'
+    //apiUrl: 'http://localhost:3000'
+    //52.207.224.130
 };
