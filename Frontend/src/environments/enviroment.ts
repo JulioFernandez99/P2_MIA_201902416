ed76e72ab63be315eec7e30d45f392289d43c0eb
Julio Fernández <98068253+JulioFernandez99@users.noreply.github.com>
2025-04-02 18:52:04 -0600

    Update enviroment.ts

--- Frontend/src/environments/enviroment.ts
+++ Frontend/src/environments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://3.237.49.68:3000'
+    apiUrl: 'http://54.172.243.160:3000'
     //apiUrl: 'http://localhost:3000'
     //52.207.224.130
 };
